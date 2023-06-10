0de233206218f96a31d6178007ee526878835c3d
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-06-09 21:16:08 -0300

    Homepage changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import "./HomePage.css"
+
 export default function HomePage(){
     return(
-        <div>
-            <h1>This is te home page of the cool site</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod consectetur animi, impedit numquam earum laborum, esse enim fugit porro omnis dolore consequuntur dolor pariatur? Quo quod possimus architecto cumque aliquam.</p>
+        <div className="home-page">
+            <div className="home-page__text-container">
+                <h1>Have you ever thought how cool it would be to have a real life minecraft block?</h1>
+                <p>We did and unfortunately it would be either impossible or not viable for a successful business. Can you even imagine how hard it would be to make a pure cube of 1m³ of diamond or gold?</p>
+                <p>Unfortunately this store is just a fake e-shop so I can practice some React.js skills  recently learned on The Odin Project course. Feel free to explore and just play with it</p>
+            </div>
         </div>
     )
 }
